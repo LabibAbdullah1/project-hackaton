626c3267e4ca65737f1f4af445b87a22da5b3c47
Labib Abdullah <labibabdullahhsan@gmail.com>
2025-12-04 22:20:50 +0700

    refactor: clean up comments in diary submission route

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,8 @@ Route::middleware('auth')->group(function () {
 
 use App\Http\Controllers\DiaryController;
 
-// Gunakan middleware 'auth' standar
+
 Route::middleware('auth')->group(function () {
-    // URL yang lebih 'web-friendly'
     Route::post('/diaries/submit', [DiaryController::class, 'store'])->name('diaries.store');
 });
 require __DIR__.'/auth.php';
